d74a0d4936db09580c5964dd87854de9e2212b45
Lê Thị Hồng Cúc <cuclth2701@gmail.com>
2021-06-09 19:02:57 +0700

    latest file

--- src/CONTROLLERS/admin/ReportManagerController.java
+++ src/CONTROLLERS/admin/ReportManagerController.java
@@ -1,16 +1,19 @@
 package CONTROLLERS.admin;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.Initializable;
 import de.jensd.fx.glyphs.materialicons.MaterialIconView;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+
+import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ReportManagerController implements Initializable {
     @FXML
-    private MaterialIconView btnPrint;
+    private JFXButton btnPrint;
 
     @FXML
     private TableView<?> tbStatistic;
@@ -26,6 +29,11 @@ public class ReportManagerController implements Initializable {
 
     @FXML
     private TableColumn<?, ?> clAmountofMoney;
+
+    @FXML
+    void PrintOnAction(ActionEvent event) {
+
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //TO DO
