be4269638ed53baeb57c9d724b16816cda282cc4
Lê Thị Hồng Cúc <cuclth2701@gmail.com>
2021-06-09 18:35:44 +0700

    latest file

--- src/CONTROLLERS/admin/ReportManagerController.java
+++ src/CONTROLLERS/admin/ReportManagerController.java
@@ -1,13 +1,34 @@
 package CONTROLLERS.admin;
 
 import javafx.fxml.Initializable;
-
+import de.jensd.fx.glyphs.materialicons.MaterialIconView;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ReportManagerController implements Initializable {
+    @FXML
+    private MaterialIconView btnPrint;
+
+    @FXML
+    private TableView<?> tbStatistic;
+
+    @FXML
+    private TableColumn<?, ?> clPeriod;
+
+    @FXML
+    private TableColumn<?, ?> clAmountofUser;
+
+    @FXML
+    private TableColumn<?, ?> clAmountofPayment;
+
+    @FXML
+    private TableColumn<?, ?> clAmountofMoney;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //TO DO
     }
 }
+
